0f6aab88a68d7fd4c724440cf683a753bf0965f8
Chutiphong012 <151540097+Chutiphong012@users.noreply.github.com>
2024-12-03 15:34:48 +0700

    a
    a

--- nextPrj/web_project/pages/index.js
+++ nextPrj/web_project/pages/index.js
@@ -2,14 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 
 export default function Home() {
   return (
     <>
+      <Navbar/>
       <h1>HomePage</h1>
       
+      <Footer/>
     </>
   );
 }
